602cfdcbd78b7817bded2d3c9497d338bf57e7d4
Sean Allesina-McGrory <allesis@wwu.edu>
2026-02-01 10:25:46 -0800

    Add ada flake
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

---
+++ ada/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A Nix-flake-based Zig development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+        ];
+
+        packages = with pkgs; [
+icon-lang
+gnumake
+        ];
+      };
+    });
+  };
+}
