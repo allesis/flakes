8f8a8e4039598b89f2cd886fbdb029afb2e263f8
Sean Allesina-McGrory <allesis@bu.edu>
2026-09-08 16:38:13 -0400

    Update easycrypt to work on mac and linux
    Also adds a shell hook which performs the solver config and setup needed
    in a new env.
    Unsure if the base package does this already but worth adding just to be
    safe.

--- easycrypt/flake.nix
+++ easycrypt/flake.nix
@@ -1,24 +1,36 @@
 {
   description = "A Nix-flake-based Easycrypt development environment";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
+
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-darwin,
   }: let
-    supportedSystems = ["x86_64-linux"];
+    supportedSystems = ["x86_64-linux" "x86_64-darwin"];
+
+    pkgsFor = system:
+    import
+    (if system == "x86_64-darwin" then nixpkgs-darwin else nixpkgs)
+    {inherit system;};
     forEachSupportedSystem = f:
       nixpkgs.lib.genAttrs supportedSystems (system:
         f {
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        });
+          pkgs = pkgsFor system;
+          });
   in {
     devShells = forEachSupportedSystem ({pkgs}: {
       default = pkgs.mkShell {
         packages = with pkgs; [
           easycrypt
+          easycrypt-runtest
         ];
+
+        shellHook = ''
+          easycrypt why3config
+        '';
       };
     });
   };
