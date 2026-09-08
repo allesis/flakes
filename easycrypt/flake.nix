895590ed081c9a829fbf2287a040ca8b1cc5e1fc
Sean Allesina-McGrory <allesis@bu.edu>
2026-09-08 16:16:02 -0400

    Add easycrypt flake

---
+++ easycrypt/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A Nix-flake-based Easycrypt development environment";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          easycrypt
+        ];
+      };
+    });
+  };
+}
