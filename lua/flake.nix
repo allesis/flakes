56e52b64c34c4a4dbc0ca39685b5f4d233a6756c
Sean Allesina-McGrory <allesis@bu.edu>
2026-09-09 11:03:48 -0400

    Update lua flake to work on x86_64-darwin

--- lua/flake.nix
+++ lua/flake.nix
@@ -1,22 +1,23 @@
 {
   description = "A Nix-flake-based Lua development environment using Lux";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
 
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-darwin,
   }: let
-    supportedSystems = ["x86_64-linux"];
+    supportedSystems = ["x86_64-linux" "x86_64-darwin"];
+
+    pkgsFor = system:
+      import
+        (if system == "x86_64-darwin" then nixpkgs-darwin else nixpkgs)
+        {inherit system;};
+
     forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        });
+      nixpkgs.lib.genAttrs supportedSystems (system: f {pkgs = pkgsFor system;});
   in {
     devShells = forEachSupportedSystem ({pkgs}: {
       default = pkgs.mkShell {
